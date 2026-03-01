f6e1100ce2137d4149b187cb34abc5788a9775eb
Josué Santos <josuesantos-jjr@github.com>
2026-03-01 10:46:50 -0300

    Corrigido fechamento do elemento main

--- src/components/manager/ManagerLayoutShell.tsx
+++ src/components/manager/ManagerLayoutShell.tsx
@@ -203,16 +203,6 @@ const ManagerLayoutShell: React.FC<ManagerLayoutShellProps> = ({ children }) => 
         </div>
 
         {/* Main Content */}
-      <main className="ml-80 p-6">
-        <motion.div
-          key={activeSection}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-        >
-          {children}
-        </motion.div>
       </main>
 
       {/* Floating Action Button for Quick Actions */}
